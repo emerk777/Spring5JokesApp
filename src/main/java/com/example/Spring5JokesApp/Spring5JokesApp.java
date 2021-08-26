e0c89948cbc28c09ef124111f434f2371a9bc25d
emerk777 <emerk777@yahoo.com>
2021-08-26 06:33:52 -0600

    New SpringBoot Project and Adding dependency guru.springframework:chuck-norris-for-actuator:2.4.0

---
+++ src/main/java/com/example/Spring5JokesApp/Spring5JokesApp.java
@@ -0,0 +1,13 @@
+package com.example.Spring5JokesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JokesApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring5JokesApp/Spring5JokesAppTest.java
@@ -0,0 +1,13 @@
+package com.example.Spring5JokesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
